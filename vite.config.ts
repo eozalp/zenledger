26750f72f3392dd7904a21a74503b49cf38523c2
Your Name <you@example.com>
2025-07-04 11:43:28 +0300

    Update base path for GitHub Pages deployment and add placeholder icons.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
+      base: '/zenledger/',
       server: {
         https: true
       },
